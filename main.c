8c252147188af081d6f0e0487072569a41d7a1ed
Miyuki Ito <mito@c1r3p9.hive.fi>
2024-02-12 14:00:12 +0200

    added split

--- main.c
+++ main.c
@@ -1,17 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/12 13:16:37 by mito              #+#    #+#             */
+/*   Updated: 2024/02/12 13:16:42 by mito             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "so_long.h"
 
 // this main func is for testing read_map func
 int main(void) 
 {
-    char *file_path = "./maps/invalid_map1.ber"; // テストファイルのパスを指定
+    char *file_path = "./maps/map1.ber"; // テストファイルのパスを指定
     char *map_str = read_map(file_path);
+    char **map_array;
     if (map_str == NULL)
     {
         printf("File could not be read or does not exist.\n");
         return (1);
     }
 	if (validate_map(map_str))
-    	printf("File content:\n%s\n", map_str);
+    {
+        map_array = ft_split(map_str, '\n');
+        // int k = 0;
+        // while (map_array[k] != '\0')
+        // {
+        //     printf("map_array:%s\n", map_array[k]);
+        //     k++;
+        // }
+    	printf("File content:\n%s\n", map_str); //delete it!
+    }
     free(map_str);
     return (0);
 }
--- map_rectangle.c
+++ map_rectangle.c
@@ -1,32 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   read_map.c                                         :+:      :+:    :+:   */
+/*   map_rectangle.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mito <mito@student.hive.fi>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/02/12 13:16:55 by mito              #+#    #+#             */
-/*   Updated: 2024/02/12 13:16:57 by mito             ###   ########.fr       */
+/*   Created: 2024/02/12 13:15:44 by mito              #+#    #+#             */
+/*   Updated: 2024/02/12 13:17:20 by mito             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "so_long.h"
-
-char *read_map(char *map)
-{
-	char	*line;
-	char	*line_joint;
-	int		fd;
-
-	fd = open(map, O_RDONLY); //read a file, fd will be -1 if read fails
-	line_joint = calloc(1, 1);
-	if (!line_joint)
-		return (0);
-	while ((line = get_next_line(fd)) != NULL)
-	{
-		line_joint = ft_strjoin(line_joint, line);
-		free (line);
-	}
-	close (fd);
-	return (line_joint);
-}
